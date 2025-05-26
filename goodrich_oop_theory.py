6e3bc4f040078f3336f1b413b11f43217cbce529
esmirnov <evgeny.smirnov@hotmail.com>
2025-05-26 22:08:58 +0300

    turnover

--- goodrich_oop_theory.py
+++ goodrich_oop_theory.py
@@ -549,3 +549,14 @@ Bob.prep_package('Alice')
 Bob.prep_package('Alice')
 Internet.check_and_queue()
 print(Alice.archived_packages)
+
+
+#Write a Python program that simulates a system that supports the functions
+#of an e-book reader. You should include methods for users of your
+#system to “buy” new books, view their list of purchased books, and read
+#their purchased books. Your system should use actual books, which have
+#expired copyrights and are available on the Internet, to populate your set
+#of available books for users of your system to “purchase” and read.
+
+
+class eBookReader
